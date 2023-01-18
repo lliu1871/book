9106ad764d056648bb57ccb7d08a18b9c24dd775
Liang Liu <lliu1871@gmail.com>
2023-01-18 12:29:49 -0500

    binf

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp ../Jupyter/jupyter_book/binf8441/_build/html/*.html ./binf8441/
+cp ../Jupyter/jupyter_book/binf8441/_build/html/_images/* ./binf8441/_images/
+git add .
+git commit -m "binf"
+git push -u origin main
