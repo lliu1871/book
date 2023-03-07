ce285e90cb0ff22822e60cba2f651542d01ed2cf
Liang Liu <lliu1871@gmail.com>
2023-03-07 08:27:53 -0500

    binf

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cp ../Jupyter/jupyter_book/$1/_build/html/*.html ./$2/
-cp ../Jupyter/jupyter_book/$1/_build/html/_images/* ./$2/_images/
-cp ../Jupyter/jupyter_book/$1/data/* ./$2/data/
+cp -r ../Jupyter/jupyter_book/$1/_build/html/_images ./$2/
+cp -r ../Jupyter/jupyter_book/$1/data ./$2/
 git add .
 git commit -m "binf"
 git push -u origin main
