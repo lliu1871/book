dc22530a0c8bd63a9e0778bcde6f999deb81cba6
lliu1871 <lliu@uga.edu>
2026-07-13 21:49:31 -0400

    Revert "Update"
    This reverts commit 2ac8b5096f97c138ad8e392b437d50e2e6be5e07.

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir $2
+cp ../jupyter_book/$1/_build/html/*.html ./$2/
+cp -r ../jupyter_book/$1/_build/html/_* ./$2/
+cp -r ../jupyter_book/$1/data ./$2/
+git add .
+git commit -m "binf"
+git push -u origin main
